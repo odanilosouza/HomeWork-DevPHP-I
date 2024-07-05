3135135ce9f7a431aa4e6f5514c3c6f0d86771b5
Danilo Souza <dev.danilosouza@gmail.com>
2024-07-05 20:42:08 -0300

    CRUD sellers + resources

--- routes/api.php
+++ routes/api.php
@@ -1,9 +1,10 @@
 <?php
 
+use App\Http\Controllers\API\SellerController;
 use App\Http\Controllers\Api\UserController;
 use Illuminate\Support\Facades\Route;
 
-Route::apiResource('/users', UserController::class);
+Route::apiResource('/', SellerController::class);
 
 Route::get('/', function () {
     return response()->json([
