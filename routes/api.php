d9f9475a1dbb725d7a8fcdf4cce75ff2a3912400
Danilo Souza <dev.danilosouza@gmail.com>
2024-07-05 21:01:16 -0300

    change controller

--- routes/api.php
+++ routes/api.php
@@ -4,10 +4,11 @@ use App\Http\Controllers\API\SellerController;
 use App\Http\Controllers\Api\UserController;
 use Illuminate\Support\Facades\Route;
 
-Route::apiResource('/', SellerController::class);
-
+Route::apiResource('/users', UserController::class);
+/*
 Route::get('/', function () {
     return response()->json([
         'success' => true
     ]);
 });
+*/
