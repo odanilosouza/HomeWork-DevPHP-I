1a7d6f273dd7b55e78135e2ffb75fde81bd39f86
Danilo Souza <dev.danilosouza@gmail.com>
2024-07-06 17:02:54 -0300

    Update method Factory and repository

--- app/Repositories/SaleRepository.php
+++ app/Repositories/SaleRepository.php
@@ -1,13 +1,33 @@
-<?php 
+<?php
 
 namespace App\Repositories;
 
-use App\Models\sale;
+use App\Models\Sale;
 
-class VendaRepository extends BaseRepository
-{
-    public function __construct(Sale $sale)
-    {
-        parent::__construct($sale);
+class SaleRepository {
+
+    protected $model;
+
+    public function __construct(Sale $model) {
+        $this->model = $model;
+    }
+
+    public function all() {
+        return $this->model->all();
+    }
+
+    public function create(array $attributes) {
+        return $this->model->create($attributes);
+    }
+
+    public function findBySellerId(int $sellerId) {
+        return $this->model->where('seller_id', $sellerId)->get();
+    }
+
+    public function delete(int $id) {
+        $sale = $this->model->find($id);
+        if ($sale) {
+            $sale->delete();
+        }
     }
 }
