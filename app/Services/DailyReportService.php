761789e8bb8adbda29bffc58fc1efd2a23fb477c
Danilo Souza <dev.danilosouza@gmail.com>
2024-07-07 14:51:11 -0300

    test pest

--- app/Services/DailyReportService.php
+++ app/Services/DailyReportService.php
@@ -9,21 +9,18 @@ class DailyReportService
 {
     public function generateReport()
     {
-        // Calcular vendas do dia
         $sales = Sale::whereDate('created_at', now())->get();
         $totalSales = $sales->sum('value_sale');
         $totalCommission = $sales->sum('commission');
 
-        // Dados para enviar no email
         $data = [
             'totalSales' => $totalSales,
             'totalCommission' => $totalCommission,
         ];
 
-        // Enviar email com o relatório
         Mail::send('emails.daily_report', $data, function ($message) {
             $message->subject('report sales daily');
-            $message->to('danilolimasza@gmail.com'); // Substitua pelo seu email ou email desejado
+            $message->to('danilolimasza@gmail.com'); 
         });
     }
 }
